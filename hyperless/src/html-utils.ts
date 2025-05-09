0373c17dd00bf85f8521fb02bdd751d4ba7ea2cc
David Bushell <david@dbushell.com>
2025-05-09 20:19:12 +0100

    extend mergeInlineNodes custom tags

--- hyperless/src/html-utils.ts
+++ hyperless/src/html-utils.ts
@@ -4,21 +4,25 @@ import { inlineTags } from "./html-tags.ts";
 /**
  * Merge adjacent text and inline Nodes into single text Node
  * @param node HTML Node
+ * @param customTags Custom inline tags
  */
-export const mergeInlineNodes = (node: Node): void => {
+export const mergeInlineNodes = (
+  node: Node,
+  customTags: Set<string> = new Set(),
+): void => {
   for (const child of [...node.children]) {
     if (child.type === "OPAQUE") {
       continue;
     }
     // Recursively merge sub-tree first
-    mergeInlineNodes(child);
+    mergeInlineNodes(child, customTags);
     // Merge this text node into the previous text node
     if (child.type === "TEXT" && child.previous?.type === "TEXT") {
       child.previous.raw += child.toString();
       child.detach();
       continue;
     }
-    if (inlineTags.has(child.tag)) {
+    if (inlineTags.has(child.tag) || customTags.has(child.tag)) {
       // Merge this inline node into previous text node
       if (child.previous?.type === "TEXT") {
         child.previous.raw += child.toString();
